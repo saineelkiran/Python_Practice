cc20ee6e846af4cf2ca9c82ac7b6ec49a8b45c6e
saineelkiran <114571119+saineelkiran@users.noreply.github.com>
2023-01-05 18:27:59 +0530

    Delete Email_Send.py

--- Email_Send.py
+++
@@ -1,8 +0,0 @@
-import smtplib
-server_1=smtplib.SMTP("smtp.gmail.com",587)
-server_1.starttls()
-server_1.ehlo()
-#Have to use App Password in GmailAccount-->Security . Instead of original password
-server_1.login("saineelkiran@gmail.com",'relojzkchswsjrvi')
-server_1.sendmail("saineelkiran@gmail.com","sravani.siri92@gmail.com","Hello Sravani, \nThis Mail is Sent using python code by Sai Neel")
-print("Mail Sent Successfully")
