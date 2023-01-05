b563b4997978ec4cdf03f608dd5b762438f5288d
Saineelkiran <saineelkiran@gmail.com>
2023-01-05 18:25:54 +0530

    change 05/01 17:30 PM

---
+++ Email_Send.py
@@ -0,0 +1,8 @@
+import smtplib
+server_1=smtplib.SMTP("smtp.gmail.com",587)
+server_1.starttls()
+server_1.ehlo()
+#Have to use App Password in GmailAccount-->Security . Instead of original password
+server_1.login("saineelkiran@gmail.com",'relojzkchswsjrvi')
+server_1.sendmail("saineelkiran@gmail.com","sravani.siri92@gmail.com","Hello Sravani, \n This Mail is Sent using python code by Sai Neel")
+print("Mail Sent Successfully")
