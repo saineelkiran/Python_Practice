45a681d9fc6550f9d5a910068d93f3749a760d25
saineelkiran <114571119+saineelkiran@users.noreply.github.com>
2023-01-05 18:37:46 +0530

    Update Email_Send_gmail.py

--- Email_Send_gmail.py
+++ Email_Send_gmail.py
@@ -3,6 +3,6 @@ server_1=smtplib.SMTP("smtp.gmail.com",587)
 server_1.starttls()
 server_1.ehlo()
 #Have to use App Password in GmailAccount-->Security . Instead of original password
-server_1.login("saineelkiran@gmail.com",'relojzkchswsjrvi')
-server_1.sendmail("saineelkiran@gmail.com","sravani.siri92@gmail.com","Hello Sravani, \nThis Mail is Sent using python code by Sai Neel")
+server_1.login("<Sender Email>",'<Gmail's App Password>')
+server_1.sendmail("<Sender Email>","<Reeiver's Email>","Hello <Receiver Name>, \nThis Mail is Sent using python code by <Sender>")
 print("Mail Sent Successfully ")
