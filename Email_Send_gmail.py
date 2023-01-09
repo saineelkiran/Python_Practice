426f8b639784d927c127941856b6ad1b5cfc20e2
Saineelkiran <saineelkiran@gmail.com>
2023-01-09 11:12:31 +0530

    change 09/01 11:12 AM
    Signed-off-by: Saineelkiran <saineelkiran@gmail.com>

--- Email_Send_gmail.py
+++ Email_Send_gmail.py
@@ -4,5 +4,5 @@ server_1.starttls()
 server_1.ehlo()
 #Have to use App Password in GmailAccount-->Security . Instead of original password
 server_1.login("saineelkiran@gmail.com",'relojzkchswsjrvi')
-server_1.sendmail("saineelkiran@gmail.com","sravani.siri92@gmail.com","Hello Sravani, \nThis Mail is Sent using python code by Sai Neel")
+server_1.sendmail("saineelkiran@gmail.com","saic90@yahoo.in","Hello Sai Neel,\n This Mail is Sent using python code by Sai Neel")
 print("Mail Sent Successfully ")
